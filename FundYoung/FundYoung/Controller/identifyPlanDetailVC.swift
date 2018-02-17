1915a4c7f502e3db3e968e84b15d990fd1cb7951
Janjao <janjao@Janjaos-MacBook-Pro-2.local>
2018-02-17 21:14:31 +0700

    done the duration pickerview

--- FundYoung/FundYoung/Controller/identifyPlanDetailVC.swift
+++ FundYoung/FundYoung/Controller/identifyPlanDetailVC.swift
@@ -8,28 +8,46 @@
 
 import UIKit
 
-class identifyPlanDetailVC: UIViewController {
+class identifyPlanDetailVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let duration = [Int](1...50)
+    @IBOutlet weak var pickerNumberOfYear: UIPickerView!
+    
+    @IBOutlet weak var SelectNumOfYear: UIButton!
+    
+   
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return duration.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(duration[row])
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        SelectNumOfYear.setTitle(String(duration[row]), for: .normal)
+        pickerNumberOfYear.isHidden = true
+    }
+    
 
+ 
+    
     override func viewDidLoad() {
+        pickerNumberOfYear.isHidden = true
+        pickerNumberOfYear.delegate = self
+        pickerNumberOfYear.dataSource = self
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func selectBtnPressed(_ sender: UIButton) {
+        if pickerNumberOfYear.isHidden{
+             pickerNumberOfYear.isHidden = false}
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
