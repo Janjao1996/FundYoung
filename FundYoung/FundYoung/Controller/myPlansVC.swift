87e57dd2ab751fb21515fdd5e8fd78d51fa05194
Janjao <janjao@Janjaos-MacBook-Pro-2.local>
2018-02-17 20:50:55 +0700

    basic user interface

---
+++ FundYoung/FundYoung/Controller/myPlansVC.swift
@@ -0,0 +1,35 @@
+//
+//  myPlansVC.swift
+//  FundYoung
+//
+//  Created by Janjao on 17/2/2561 BE.
+//  Copyright © 2561 Janjao. All rights reserved.
+//
+
+import UIKit
+
+class myPlansVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
